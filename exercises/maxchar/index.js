b0b065b3bc53d5728172688138c57f3c9247f06b
edensongjbs <edensongjbs@gmail.com>
2020-10-15 14:01:06 -0400

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charObj = {}
+    for (const char of str) {
+        charObj[char] = charObj[char]++ || 1
+    }
+    let max={val:0}
+    for (const numOf in charObj) {
+        if (charObj[numOf]>max.val) {
+            max={key:numOf, val:charObj[numOf]}
+        }
+    }
+    return max.key
+}
 
 module.exports = maxChar;
