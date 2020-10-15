168d9d7b75959331445f2c4e140f1c9a32fe0870
edensongjbs <edensongjbs@gmail.com>
2020-10-15 13:28:42 -0400

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let mult=1, rev=0, sign=Math.sign(n)
+    n=n*sign
+    i=n
+    while (i>=10) {
+        console.log(i)
+        mult *= 10
+        i /=10
+    }
+    while (mult>=1) {
+        let dig=n%10
+        n=Math.floor(n/10)
+        rev+=mult*dig
+        mult/=10
+    }
+    console.log(rev)
+    return rev*sign
+}
+
+reverseInt(-15)
 
 module.exports = reverseInt;
