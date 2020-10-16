25bb80bae9d2600f9de4b98a51e2bd9493f3372e
edensongjbs <edensongjbs@gmail.com>
2020-10-16 13:44:54 -0400

    spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,36 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const m = []
+    for (let i=0; i<n; i++){
+        m.push([])
+    }
+    let count=1, startRow=0, endRow=n-1, startCol=0, endCol=n-1
+    while (startCol <= endCol && startRow <= endRow) {
+        for (let col=startCol; col <= endCol; col++) {
+            m[startRow][col]=count
+            count++
+        }
+        startRow++
+        for (let row=startRow; row <= endRow; row++) {
+            m[row][endCol]=count
+            count++
+        }
+        endCol--
+        for (let col=endCol; col >= startCol; col--) {
+            m[endRow][col]=count
+            count++
+        }
+        endRow--
+        for (let row=endRow; row >= startRow; row--) {
+            m[row][startCol]=count
+            count++
+        }
+        startCol++
+    }
+    console.log(m)
+    return m
+}
 
 module.exports = matrix;
