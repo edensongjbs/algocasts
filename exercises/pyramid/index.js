12da47384fa327365321fc74ca8b2876a1f312de
edensongjbs <edensongjbs@gmail.com>
2020-10-16 12:34:23 -0400

    pyramid 2 solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,32 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row=0, str='') {
+    if (row===n) {return}
+    if (str.length===2*n-1) {
+        console.log(str)
+        return pyramid(n, row+1)
+    }
+    if (str.length<n-1-row || str.length>n-1+row){
+        str+=' '
+        pyramid(n, row, str)
+    }
+    else {
+        str+='#'
+        pyramid(n, row, str)
+    }
+}
+
+// function pyramid(n) {
+//     startAr = []
+//     for (let i=0; i<2*n-1; i++) {
+//         startAr.push(' ')
+//     }
+//     for (let i=0; i<n; i++) {
+//         startAr[n-1+i]="#"
+//         startAr[n-1-i]="#"
+//         console.log(startAr.join(''))
+//     }
+// }
 
 module.exports = pyramid;
