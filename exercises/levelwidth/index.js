e817a4653b9ee55971c449a805ba6745b632be7d
edensongjbs <edensongjbs@gmail.com>
2020-10-19 17:53:59 -0400

    finish levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let widths=[0], arr=[root, 's'], lev=0, node
+    while (arr.length){
+        node=arr.shift()
+        if (node==='s') {
+            if (arr.length) {
+                widths.push(0)
+                lev++
+                arr.push('s')
+            }
+       }
+       else {
+           arr.push(...node.children)
+           widths[lev]++
+       }
+    }
+    console.log(widths)
+    return widths
+    //can do this without the lev variable (just by always accesssing last element in widths ar)
+}
 
 module.exports = levelWidth;
