47453a817c2ad0282686cb2d6ea2c80cd1c4a122
edensongjbs <edensongjbs@gmail.com>
2020-10-19 15:54:48 -0400

    finish midpoint

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,14 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+    let slow=list.head, fast=list.head
+    if (!fast){return null}
+    while (fast.next && fast.next.next) {
+        fast=fast.next.next
+        slow=slow.next
+    }
+    return slow
+}
 
 module.exports = midpoint;
