f0bfce5e4a97e8ba38cbd5626b25ddb27d8b8883
edensongjbs <edensongjbs@gmail.com>
2020-10-21 11:17:56 -0400

    implement selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -17,7 +17,16 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+    for (i=0; i<arr.length-1; i++) {
+        for (j=i+1; j<arr.length; j++) {
+            if (arr[j]<arr[i]) {
+                const temp=arr[j]
+                arr[j]=arr[i]
+                arr[i]=temp
+            }
+        }
+    }
+    return arr
 }
 
 function mergeSort(arr) {
